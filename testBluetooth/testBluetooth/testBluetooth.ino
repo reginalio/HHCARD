63578c25a8fc6ddcfb97486f702d90561f077bc3
Regina Lio <regina_lio@hotmail.com>
2015-03-24 14:31:05 +0000

    Added test bluetooth code

---
+++ testBluetooth/testBluetooth/testBluetooth.ino
@@ -0,0 +1,51 @@
+int led = 8;
+
+void setup() {
+  pinMode(led,OUTPUT);
+  digitalWrite(led, LOW);
+  
+  // I read online that you may need to use Serial1 instead of Serial
+  // if you are using the Arduino micro for rx input
+  // (Serial worked fine for Arduino Uno)
+  Serial.begin(9600); 
+  Serial.flush(); 
+}
+
+void loop() {
+  // Check bluetooth input if available, else just blink
+  if(Serial.available() > 0){
+    char input = (char)Serial.read();
+    Serial.println(input);
+    
+    // if input is A, keep LED on until input changes
+    if(input == 'A'){
+      digitalWrite(led, HIGH);
+      Serial.println("I see A");    
+      while(1){
+        Serial.println("Waiting to blink again");
+        Serial.println(input);
+        if(Serial.available() > 0){
+           input = (char)Serial.read();
+           Serial.println("I read");
+           Serial.println(input);
+           if(input != 'A'){
+             break;
+           }
+        }else{
+          delay(100);
+        }
+      }
+    }
+  }
+  else{
+    // input not available
+    blinkLED(led);
+  }
+}
+
+void blinkLED(int thatLED){
+    digitalWrite(thatLED, HIGH);
+     delay(200);  
+     digitalWrite(thatLED,LOW);
+     delay(200);
+}
